c07f3439314bff9f3aaa5700a9cd9ae446f456da
Mzs Munna <mzs.munna@gmail.com>
2020-05-12 02:18:49 +0600

    Another Entity + Reposiory added but Interface missing for all new

---
+++ MHN.Sync.Entity/MongoEntity/EnrollmentRequest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHN.Sync.Entity.MongoEntity
+{
+    public class EnrollmentRequest : IEntity
+    {
+        public string Id { get; set; }
+        public string ProspectMemberDataRef { get; set; }
+        public string EnrollInPlan { get; set; }
+        public string EnrollPCPName { get; set; }
+        public string EnrollPCPId { get; set; }
+        public string MedicareCardName { get; set; }
+        public string MedicareNumber { get; set; }
+        public string MedicarePartA { get; set; }
+        public string MedicarePartB { get; set; }
+        public string PremiumPaymentOption { get; set; }
+        public string MonthlyBenefitFrom { get; set; }
+        public string HaveESRD { get; set; }
+        public string HaveOtherDrugCoverage { get; set; }
+        public DrugCoverage DrugCoverage { get; set; }
+        public bool IsLongTermCareResident { get; set; }
+        public LongTermCareResident LongTermCareResident { get; set; }
+        public bool IsEnrolledStateMedicaidProgram { get; set; }
+        public string MedicaidNumber { get; set; }
+        public bool IsSpouseWork { get; set; }
+        public string PreferedLanguage { get; set; }
+        public AuthorizedRepresentative AuthorizedRepresentative { get; set; }
+        public string AgentName { get; set; }
+        public string AgentId { get; set; }
+        public string EffectiveDateCoverage { get; set; }
+        public string EventId { get; set; }
+        public string PreferredDayOutreach { get; set; }
+
+        //public bool IsActive { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+
+    public class DrugCoverage
+    {
+        public string CoverageName { get; set; }
+        public string CoverageId { get; set; }
+        public string CoverageGroup { get; set; }
+
+    }
+
+    public class LongTermCareResident
+    {
+        public string InstitutionName { get; set; }
+        public string InstitutionAddress { get; set; }
+        public string PhoneNumber { get; set; }
+
+    }
+
+    public class AuthorizedRepresentative
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string RelationshipToEnrollee { get; set; }
+    }
+}
---
+++ MHN.Sync.MongoRepository/EnrollmentRequestRepository.cs
@@ -0,0 +1,180 @@
+﻿using MHN.Sync.Entity.MongoEntity;
+using MHN.Sync.MongoInterface;
+using MHN.Sync.MongoInterface.BASE;
+using MHN.Sync.MongoRepository.DAL;
+using MHN.Sync.MongoRepository.Serializar;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHN.Sync.MongoRepository
+{
+    public class EnrollmentRequestRepository : RepositoryBase //, IEnrollmentRequestRepository
+    {
+        private static IMongoCollection<EnrollmentRequest> Collection { get; set; }
+        private MongoDBCore<EnrollmentRequest> core;
+        static EnrollmentRequestRepository()
+        {
+            if (!BsonClassMap.IsClassMapRegistered(typeof(EnrollmentRequest)))
+            {
+                BsonClassMap.RegisterClassMap<EnrollmentRequest>(map =>
+                {
+                    map.AutoMap();
+                    map.SetIgnoreExtraElements(true);
+                    map.MapProperty(x => x.Id).SetElementName("_id");
+                    map.GetMemberMap(x => x.Id).SetSerializer(new StringSerializer(BsonType.ObjectId));
+
+                });
+            }
+        }
+
+        public EnrollmentRequestRepository(IDatabaseContext dbContext)
+            : base(dbContext)
+        {
+            try
+            {
+                if (Collection == null)
+                {
+                    Collection = _database.GetCollection<EnrollmentRequest>("EnrollmentRequest");
+                }
+                core = new MongoDBCore<EnrollmentRequest>(Collection);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private FilterDefinition<EnrollmentRequest> BuildFilter(string _id)
+        {
+            var filter = Builders<EnrollmentRequest>.Filter.Empty;
+
+            if (!string.IsNullOrEmpty(_id) && _id.ToLower() != "undefined")
+            {
+                filter = filter & Builders<EnrollmentRequest>.Filter.Eq("_id", _id);
+            }
+
+            return filter;
+        }
+
+        public async Task<EnrollmentRequest> GetById(string _id)
+        {
+            var filter = BuildFilter(_id);
+            return await Collection.Find(filter).FirstOrDefaultAsync().ConfigureAwait(false);
+
+        }
+
+        public async Task<List<EnrollmentRequest>> GetAllByField(string fieldName, string fieldValue)
+        {
+            var filter = Builders<EnrollmentRequest>.Filter.Eq(fieldName, fieldValue);
+            var result = await Collection.Find(filter).ToListAsync().ConfigureAwait(false);
+            //var result = await Collection.Find(filter).FirstOrDefaultAsync().ConfigureAwait(false);
+            return result;
+        }
+
+        public async Task<List<EnrollmentRequest>> GetAll()
+        {
+            var filter = BuildFilter(null);
+            return await Collection.Find(filter).ToListAsync().ConfigureAwait(false);
+        }
+
+        public async Task<List<EnrollmentRequest>> GetAll(int currentPage, int pageSize)
+        {
+            var filter = BuildFilter(null);
+            return await Collection.Find(filter).Skip(currentPage * pageSize).Limit(pageSize).ToListAsync().ConfigureAwait(false);
+        }
+
+        public int GetAllCount()
+        {
+            int count = 0;
+            try
+            {
+                var filter = BuildFilter(null);
+                count = Convert.ToInt32(Collection.Find(filter).Count());
+            }
+            catch (Exception ex)
+            {
+                //new ExceptionWrapper(ex).Handle();
+            }
+            return count;
+        }
+
+        public EnrollmentRequest GetPtcRequestDetailsById(string prosMemberRef)
+        {
+            try
+            {
+                var filter = Builders<EnrollmentRequest>.Filter.Eq("ProspectMemberDataRef", prosMemberRef);
+                return Collection.Find(filter).FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        public string Save(IEntity entity)
+        {
+            var returnVal = string.Empty;
+            try
+            {
+                MongoDbOperationResult result = new MongoDBHelper<EnrollmentRequest>(Collection).Save(entity).Result;
+                returnVal = result.Id;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return returnVal;
+        }
+
+        public string SaveMany(IEnumerable<EnrollmentRequest> records)
+        {
+            var returnVal = string.Empty;
+
+            try
+            {
+                Collection.InsertMany(records);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return returnVal;
+        }
+
+        public bool? Delete(IEntity entity)
+        {
+            bool? returnVal = null;
+
+            try
+            {
+                EnrollmentRequest _actualEntity = entity as EnrollmentRequest;
+                //_actualEntity.IsDeleted = true;
+                //_actualEntity.ModifiedOn = DateTime.Now;
+
+                MongoDbOperationResult result = new MongoDBHelper<EnrollmentRequest>(Collection).Save(_actualEntity).Result;
+
+                returnVal = string.IsNullOrEmpty(result.Id) ? false : true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                //Do Nothing
+            }
+
+            return returnVal;
+        }       
+    }
+}
