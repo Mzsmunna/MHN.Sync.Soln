55dc008ded8ffbb8c7a0fd391d92733fc921c4d3
Mzs Munna <mzs.munna@gmail.com>
2020-07-19 01:49:19 +0600

    JobUtility Added from GTW

---
+++ MHN.Sync.Entity/MongoEntity/DataSyncJobList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHN.Sync.Entity.MongoEntity
+{
+    public class DataSyncJobList: IEntity
+    {
+        public string Id { get; set; }
+        public string CampaignId { get; set; }
+        public string ClientId { get; set; }
+        public string SyncType { get; set; } //SyncType enum
+        public string SyncStatus { get; set; } //SyncStatus enum
+        public int ExecutionStartHour { get; set; } // this will be in 24 hour
+        public int ExecutionEndHour { get; set; } // this will be in 24 hour
+        public string ScheduleExpression { get; set; }
+        public string FileLocation { get; set; }
+        public string FileSource { get; set; }
+        public Boolean IsActive { get; set; }
+        public int OrderBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
---
+++ MHN.Sync.Jobs/JobUtility.cs
@@ -0,0 +1,43 @@
+﻿using MHN.Sync.Entity.MongoEntity;
+using NCrontab;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHN.Sync.Jobs
+{
+    public static class JobUtility
+    {
+
+        public static List<DataSyncJobList> FindExecutableJobs(List<DataSyncJobList> jobList)
+        {
+            List<DataSyncJobList> executableJobList = new List<DataSyncJobList>();
+
+            var timeUtc = DateTime.UtcNow;
+            TimeZoneInfo easternZone = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+            DateTime easternTime = TimeZoneInfo.ConvertTimeFromUtc(timeUtc, easternZone);
+            var oneHourBefore = easternTime.AddHours(-1);
+            foreach (var job in jobList)
+            {
+                if (!string.IsNullOrEmpty(job.ScheduleExpression))
+                {
+                    var schedule = CrontabSchedule.Parse(job.ScheduleExpression);
+                    var exicutionTime = schedule.GetNextOccurrence(oneHourBefore);
+                    var exicutionHour = exicutionTime.ToString("MM-dd-yyyy-")+ exicutionTime.Hour;
+                    var currentHour = easternTime.ToString("MM-dd-yyyy-") + easternTime.Hour;
+                    if (exicutionHour == currentHour)
+                    {
+                        executableJobList.Add(job);
+                        Console.WriteLine("Name : " + job.SyncType + ", Time : " + job.ScheduleExpression);
+                    }
+                }
+                else
+                    if (easternTime.Hour == job.ExecutionStartHour)
+                    executableJobList.Add(job);
+            }
+            return executableJobList;
+        }
+    }
+}
